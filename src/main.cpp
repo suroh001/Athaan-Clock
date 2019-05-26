93461956bf317eb5c5c5243ce289cd2d4e4969f7
Habeeb <habeeb@Habeebs-Mac-mini.local>
2019-05-26 10:48:37 +0100

    0.01

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+
+//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, 13, 11, 10, 8);
+U8G2_ST7920_128X64_F_8080 u8g2(U8G2_R0, 2, 3, 4, 5, 6, 7, 8, 9, 13, U8X8_PIN_NONE, 12, 11);
+
+//U8X8_PIN_NONE
+/*
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0,24,"Hello World!");
+  } while ( u8g2.nextPage() );
+}
+*/
+
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(0,32,"AY WHAT'S UP");
+  u8g2.sendBuffer();
+}
